feaf732aa17c4273ff90ff46a306a402a7fb806c
Kanhpahm <107665017+Kanhpahm@users.noreply.github.com>
2022-09-28 11:22:23 +0700

    create thumb for product

--- app/Services/Products/ProductServices.php
+++ app/Services/Products/ProductServices.php
@@ -4,6 +4,7 @@ namespace App\Services\Products;
 
 use App\Models\Menu;
 use App\Models\Product;
+use Illuminate\Support\Facades\Session;
 
 class ProductServices
 {
@@ -16,5 +17,44 @@ class ProductServices
     {
         return Product::with('menu')->select('id', 'name', 'menu_id', 'thumb', 'active')
             ->orderByDesc('id')->paginate(5);
+    }
+
+    public function create($request)
+    {
+        try {
+            $product = new Product();
+            $product->fill($request->all());
+            if ($request->hasFile('thumb')) {
+                $image = $request->thumb;
+                $imageName = $image->hashName();
+                $imageName = $request->name . '_' . $imageName;
+                $product->thumb = $image->storeAs('images/products', $imageName);
+                $product->content = $request->content;
+                $product->name = $request->name;
+                $product->price_id = $request->price_id;
+                $product->menu_id = $request->menu_id;
+                $product->active = $request->active;
+                $product->save();
+            }
+
+            $productId = $product->id;
+//            if ($request->hasFile('thumb')) {
+//                foreach ($request->thumb as $file) {
+//                    $imageNew = new Thumb();
+//                    if (isset($file)) {
+//                        $imageNew->gallery = $file->hashName();
+//                        $imageNew->product_id = $productId;
+//                        // $imageNew = $file->storeAs('images/products', $imageNew);
+//                        $file->move('images/products', $file->hashName());
+//                        $imageNew->save();
+//                    }
+//                }
+//            }
+            Session::flash('success', 'Tạo mới thành công');
+            return redirect()->route('products.index');
+        } catch (\Exception $err) {
+            Session::flash('error', $err->getMessage());
+            return false;
+        }
     }
 }
